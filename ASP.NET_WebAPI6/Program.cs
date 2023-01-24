1e4ee777de203d4f49878514da1cc1155a3ed0bc
LUIS MIGUEL GUERRERO GUTIERREZ <luguerrero10@poligran.edu.co>
2023-01-24 14:53:29 -0500

    Settings Web API

---
+++ ASP.NET_WebAPI6/Program.cs
@@ -0,0 +1,33 @@
+using ASP.NET_WebAPI6.Entities;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEntityFrameworkMySQL()
+                .AddDbContext<DBContext>(options =>
+                {
+                    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+                });
+builder.Services.AddControllers();
+ 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
